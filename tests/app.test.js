413aa37a3cd9ca432fd92a0199ae138b54802fbd
VijayGite-duplocloud <vijay.gite@duplocloud.net>
2025-07-24 11:57:15 +0530

    changes in test file

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,10 +1,16 @@
 // tests/app.test.js
-const app = require('../src/app');
-const request = require('supertest');
+// const app = require('../src/app');
+// const request = require('supertest');
 
-describe('App Test', () => {
-  it('should return Hello World', async () => {
-    const res = await request(app).get('/');
-    expect(res.text).toBe('Goodbye World'); // Intentional fail
+// describe('App Test', () => {
+//   it('should return Hello World', async () => {
+//     const res = await request(app).get('/');
+//     expect(res.text).toBe('Goodbye World'); // Intentional fail
+//   });
+// });
+// src/test.js
+describe('Demo Test', () => {
+  it('should fail', () => {
+    throw new Error('Intentional test failure for demo');
   });
 });
