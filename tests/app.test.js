628ad3ff8e71743f0f6abd619c7e414ad9a1c001
VijayGite-duplocloud <vijay.gite@duplocloud.net>
2025-07-24 14:50:45 +0530

    removed unwanted comment

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,6 +4,6 @@ describe('API Test', () => {
   it('should return 200 and expected data', async () => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
     expect(response.status).toBe(200);
-    expect(response.data).toHaveProperty('nonExistentField'); // Intentional failure
+    expect(response.data).toHaveProperty('nonExistentField');
   });
 });
